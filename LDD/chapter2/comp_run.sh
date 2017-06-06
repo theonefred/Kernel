7e1306e6523af6088a77d69f5880652d145ee449
theonefred <theonefred@users.noreply.github.com>
2017-06-06 14:20:45 +0800

    remove unknown parameter

--- LDD/chapter2/comp_run.sh
+++ LDD/chapter2/comp_run.sh
@@ -2,7 +2,8 @@
 make
 #insmod ./helloworld.ko
 #insmod ./helloworld.ko age=15 name="NEW_NAME"
-insmod ./helloworld.ko age=15 number=20 name="NEW_NAME" arr=1,2,3
+#insmod ./helloworld.ko age=15 number=20 name="NEW_NAME" arr=1,2,3 #with unknown parameter age, other parameters will remain the default values
+#insmod ./helloworld.ko number=20 name="NEW_NAME" arr=1,2,3
 dmesg|tail -10
 echo
 echo "=====insmod done====="
